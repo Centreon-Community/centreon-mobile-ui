dcb085cb213824525e983ed812fa352f87f213c3
nutz <nutz>
2012-12-20 22:52:30 +0000

    git-svn-id: http://svn.modules.centreon.com/mobile-ui/tags/1.0b@9 a4990d27-fff2-4a1c-9e3e-bda8f4c22f0a

--- include/services.php
+++ include/services.php
@@ -263,29 +263,10 @@ else {
 
 $result_service_status = mysql_query ($query_service_status);
 
-
+include ("header.php");
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Centreon - IT & Network Monitoring</title>
-	<link rel="shortcut icon" href="./img/favicon.ico"/>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="robots" content="index, nofollow" />
-	<!--<meta name="viewport" content="width=479px" />-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-<!--Import CSS & Javascript-->	
-	<link href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" rel="stylesheet" type="text/css" />
-	<link href="../css/style-mobile.css" rel="stylesheet" type="text/css"/>
-	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script type="text/javascript" src="js/mobile-UI.js"></script>
-	<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
-
-</head>
-<body>
     <div data-role="page">
 	
         <div data-role="header" data-theme="<?php echo $theme;?>">
