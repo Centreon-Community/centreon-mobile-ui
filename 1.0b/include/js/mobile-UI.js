50b9c2d4955f4063819f3af37a562be79d014e21
nutz <nutz>
2012-12-20 23:17:23 +0000

    git-svn-id: http://svn.modules.centreon.com/mobile-ui/branches/1.x@10 a4990d27-fff2-4a1c-9e3e-bda8f4c22f0a

--- 1.0b/include/js/mobile-UI.js
+++ 1.0b/include/js/mobile-UI.js
@@ -1,20 +1,9 @@
 /*Personnal JS functions
 /Including jquery-mobile global configuration override
-*/
-$(document).on('pagehide', function (e) {
-    var page = $(e.target);
-    if (!$.mobile.page.prototype.options.domCache
-        && (!page.attr('data-dom-cache')
-            || page.attr('data-dom-cache') == "false")
-        ) {
-        page.remove();
-    }
-	
-	});
 
 $(document).bind('mobileinit',function(){
-   $.mobile.selectmenu.prototype.options.nativeMenu = false;
+   /*$.mobile.selectmenu.prototype.options.nativeMenu = false;*/ /*ne pas activer sinon le reload sur menus déroulants ne fonctionne plus*/
    $.mobile.page.prototype.options.domCache = false;
    $.mobile.ajaxEnabled = true;
-   $.mobile.changePage.defaults.reloadPage = false;
+   $.mobile.changePage.defaults.reloadPage = true;
 });
