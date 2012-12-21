8c6866093e51b4251420e3d7cde4b3b73062e858
nutz <nutz>
2012-12-21 05:43:59 +0000

    git-svn-id: http://svn.modules.centreon.com/mobile-ui/tags/1.0b@11 a4990d27-fff2-4a1c-9e3e-bda8f4c22f0a

--- include/service_details.php
+++ include/service_details.php
@@ -8,70 +8,30 @@ require_once "include_first.php";
 //--------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------------
-include_once "common.php";
 
+/*on inclus les fichiers communs*/
+include_once "common.php";
+include "queries/queries_common.php";
+
+/*récupération des variables passées en url*/
 $host_id = $_GET['host_id'];
 $service_id = $_GET['service_id'];
 $servicestatus_id = $_GET['status_id'];
+$hostname = $_GET['hostname'];
+$service = $_GET['service'];
 
-$query_details_service =	'SELECT 
-								'.$ndoDB_assoc["db_prefix"].'hosts.alias,
-								'.$ndoDB_assoc["db_prefix"].'hosts.display_name AS host_display_name,
-								'.$ndoDB_assoc["db_prefix"].'services.service_id,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.servicestatus_id,
-								'.$ndoDB_assoc["db_prefix"].'services.display_name,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.output,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.perfdata,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.current_state,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.has_been_checked,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.current_check_attempt,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.max_check_attempts,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_check,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.next_check,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_state_change,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.check_type,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_hard_state_change,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_hard_state,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_ok,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_warning,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_unknown,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_critical,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.state_type,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.execution_time,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.notifications_enabled,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.problem_has_been_acknowledged,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.acknowledgement_type,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.active_checks_enabled,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.passive_checks_enabled,
-								'.$ndoDB_assoc["db_prefix"].'servicestatus.latency,
-								'.$ndoDB_assoc["db_prefix"].'acknowledgements.acknowledgement_id,
-								'.$ndoDB_assoc["db_prefix"].'hosts.config_type
-							FROM    
-								(('.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'services '.$ndoDB_assoc["db_prefix"].'services
-							INNER JOIN
-								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'hosts '.$ndoDB_assoc["db_prefix"].'hosts
-							ON 
-								('.$ndoDB_assoc["db_prefix"].'services.host_object_id =
-								'.$ndoDB_assoc["db_prefix"].'hosts.host_object_id))
-							INNER JOIN
-								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'servicestatus '.$ndoDB_assoc["db_prefix"].'servicestatus
-							ON 
-								('.$ndoDB_assoc["db_prefix"].'servicestatus.service_object_id =
-								'.$ndoDB_assoc["db_prefix"].'services.service_object_id))
-							LEFT OUTER JOIN
-								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'acknowledgements '.$ndoDB_assoc["db_prefix"].'acknowledgements
-							ON 
-								('.$ndoDB_assoc["db_prefix"].'services.host_object_id = '.$ndoDB_assoc["db_prefix"].'acknowledgements.object_id)
-							WHERE    
-								('.$ndoDB_assoc["db_prefix"].'services.service_id = '.$service_id.')
-							AND 
-								('.$ndoDB_assoc["db_prefix"].'servicestatus.servicestatus_id = '.$servicestatus_id.')
-							AND 
-								('.$ndoDB_assoc["db_prefix"].'hosts.config_type = 0)';
+/*sélection du fichier contenant les requetes*/
+include $Broker_queries_path.'queries_service_details.php';
 
+/*on execute les requetes nécessaires à la page*/	
 $result_details_service = mysql_query ($query_details_service);
+$result_metric_details = mysql_query ($query_metric_details);
 $obj_details_service = mysql_fetch_object ($result_details_service);
+$obj_metric_details = mysql_fetch_object ($result_metric_details);
+$result_session_id = mysql_query ($query_session_id);
+$obj_session_id = mysql_fetch_object ($result_session_id);
 
+/*on inclus le header*/
 include ("header.php");
 ?>
 
@@ -186,6 +146,8 @@ HTML
 				<br />
 				<font>
 				<?php echo ''.$obj_details_service->output.''; ?>
+				<br />
+				<img src="../../../include/views/graphs/generateGraphs/generateImage.php?session_id=<?php echo $obj_session_id->session_id; ?>&index=<?php echo $obj_metric_details->id; ?>&end=<?php echo time(); ?>&start=<?php echo time()-(60*60*24); ?>">
 				</font>
 			</div>
 		</div>
