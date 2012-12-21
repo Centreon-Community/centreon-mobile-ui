27dd4090a72b5cf846fc06b6dfeba1ab1ee6dfcc
nutz <nutz>
2012-12-21 05:46:07 +0000

    git-svn-id: http://svn.modules.centreon.com/mobile-ui/tags/1.0b@12 a4990d27-fff2-4a1c-9e3e-bda8f4c22f0a

---
+++ include/queries/ndo/queries_mobile-UI_1.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*NDO*/
+
+//>>---------------------------<<//
+///////////////////////////////////
+// requests used in mobile-UI.php /
+///////////////////////////////////
+//>>---------------------------<<//
+
+	$query_total_hosts =					'SELECT 
+											'.$ndoDB_assoc["db_prefix"].'hosts.config_type, 
+											'.$ndoDB_assoc["db_prefix"].'hosts.alias
+											FROM '.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'hoststatus '.$ndoDB_assoc["db_prefix"].'hoststatus
+											INNER JOIN
+											'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'hosts '.$ndoDB_assoc["db_prefix"].'hosts
+											ON ('.$ndoDB_assoc["db_prefix"].'hoststatus.host_object_id = '.$ndoDB_assoc["db_prefix"].'hosts.host_object_id)
+											WHERE ('.$ndoDB_assoc["db_prefix"].'hosts.config_type = 0)';
+								
+	$query_total_services =					'SELECT '.$ndoDB_assoc["db_prefix"].'servicestatus.current_state,
+											'.$ndoDB_assoc["db_prefix"].'services.config_type,
+											'.$ndoDB_assoc["db_prefix"].'hosts.config_type
+											FROM ('.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'services '.$ndoDB_assoc["db_prefix"].'services
+											INNER JOIN
+											'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'hosts '.$ndoDB_assoc["db_prefix"].'hosts
+											ON ('.$ndoDB_assoc["db_prefix"].'services.host_object_id = '.$ndoDB_assoc["db_prefix"].'hosts.host_object_id))
+											INNER JOIN
+											'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'servicestatus '.$ndoDB_assoc["db_prefix"].'servicestatus
+											ON ('.$ndoDB_assoc["db_prefix"].'servicestatus.service_object_id =
+											'.$ndoDB_assoc["db_prefix"].'services.service_object_id)
+											WHERE ('.$ndoDB_assoc["db_prefix"].'services.config_type = 0) AND ('.$ndoDB_assoc["db_prefix"].'hosts.config_type = 0)';
+
+?>
---
+++ include/queries/ndo/queries_service_details.php
@@ -0,0 +1,71 @@
+<?php
+
+/*NDO*/
+
+//>>---------------------------------<<//									
+/////////////////////////////////////////
+// requests used in service_details.php /
+/////////////////////////////////////////
+//>>---------------------------------<<//
+
+	$query_details_service =	'SELECT 
+								'.$ndoDB_assoc["db_prefix"].'hosts.alias,
+								'.$ndoDB_assoc["db_prefix"].'hosts.display_name AS host_display_name,
+								'.$ndoDB_assoc["db_prefix"].'services.service_id,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.servicestatus_id,
+								'.$ndoDB_assoc["db_prefix"].'services.display_name,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.output,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.perfdata,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.current_state,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.has_been_checked,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.current_check_attempt,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.max_check_attempts,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_check,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.next_check,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_state_change,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.check_type,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_hard_state_change,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_hard_state,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_ok,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_warning,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_unknown,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.last_time_critical,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.state_type,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.execution_time,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.notifications_enabled,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.problem_has_been_acknowledged,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.acknowledgement_type,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.active_checks_enabled,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.passive_checks_enabled,
+								'.$ndoDB_assoc["db_prefix"].'servicestatus.latency,
+								'.$ndoDB_assoc["db_prefix"].'acknowledgements.acknowledgement_id,
+								'.$ndoDB_assoc["db_prefix"].'hosts.config_type
+							FROM    
+								(('.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'services '.$ndoDB_assoc["db_prefix"].'services
+							INNER JOIN
+								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'hosts '.$ndoDB_assoc["db_prefix"].'hosts
+							ON 
+								('.$ndoDB_assoc["db_prefix"].'services.host_object_id =
+								'.$ndoDB_assoc["db_prefix"].'hosts.host_object_id))
+							INNER JOIN
+								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'servicestatus '.$ndoDB_assoc["db_prefix"].'servicestatus
+							ON 
+								('.$ndoDB_assoc["db_prefix"].'servicestatus.service_object_id =
+								'.$ndoDB_assoc["db_prefix"].'services.service_object_id))
+							LEFT OUTER JOIN
+								'.$ndoDB_assoc["db_name"].'.'.$ndoDB_assoc["db_prefix"].'acknowledgements '.$ndoDB_assoc["db_prefix"].'acknowledgements
+							ON 
+								('.$ndoDB_assoc["db_prefix"].'services.host_object_id = '.$ndoDB_assoc["db_prefix"].'acknowledgements.object_id)
+							WHERE    
+								('.$ndoDB_assoc["db_prefix"].'services.service_id = '.$service_id.')
+							AND 
+								('.$ndoDB_assoc["db_prefix"].'servicestatus.servicestatus_id = '.$servicestatus_id.')
+							AND 
+								('.$ndoDB_assoc["db_prefix"].'hosts.config_type = 0)';
+
+$query_metric_details = 	'SELECT index_data.id
+							FROM centstorage.index_data
+							WHERE (index_data.host_name = "'.$hostname.'")
+							AND (index_data.service_description = "'.$service.'")';
+
+?>
