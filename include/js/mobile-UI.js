eb4096967a51827018234b983e1197f666850bc9
nutz <nutz>
2012-12-25 23:28:47 +0000

    git-svn-id: http://svn.modules.centreon.com/mobile-ui/tags/1.0b@15 a4990d27-fff2-4a1c-9e3e-bda8f4c22f0a

--- include/js/mobile-UI.js
+++ include/js/mobile-UI.js
@@ -1,5 +1,5 @@
 /*Personnal JS functions
-/Including jquery-mobile global configuration override
+/Including jquery-mobile global configuration override*/
 
 $(document).bind('mobileinit',function(){
    /*$.mobile.selectmenu.prototype.options.nativeMenu = false;*/ /*ne pas activer sinon le reload sur menus déroulants ne fonctionne plus*/
@@ -7,3 +7,22 @@ $(document).bind('mobileinit',function(){
    $.mobile.ajaxEnabled = true;
    $.mobile.changePage.defaults.reloadPage = true;
 });
+
+function affiche_jour(id){
+	document.getElementById('graphe_img_jour').style.display='block';
+	}
+function affiche_mois(id){
+	document.getElementById('graphe_img_mois').style.display='block';
+	}
+function affiche_an(id){
+	document.getElementById('graphe_img_an').style.display='block';
+	}
+function cache_jour(id){
+	document.getElementById('graphe_img_jour').style.display='none';
+	}
+function cache_mois(id){
+	document.getElementById('graphe_img_mois').style.display='none';
+	}
+function cache_an(id){
+document.getElementById('graphe_img_an').style.display='none';
+	}
